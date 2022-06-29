2b312883ace94c21bc0e23e1f570df635436b7b7
Michael Bishop Work PC <mrbishop5544@gmail.com>
2022-06-29 15:05:51 -0400

    changes to spacing for html for readibility, and js file start

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,21 @@
-// Assignment code here
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+// Start of password generating code and prompts
+function generatePassword {
+
+  // Array list for all Characters / Numbers / Symbols acceptable by type
+var uppers = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var lowers = ["abcdefghijklmnopqrstuvwxyz"];
+var numbers = ["0123456789"];
+var symbols = ["!@#$%^&*()_-[]{}"];
+
+  // Initial Window Prompt asking about password length
+  var passLength = window.prompt("How many characters will the password length be? Please select a length between 8 and 128 characters long!");
+
+
+
+}
 
 // Write password to the #password input
 function writePassword() {
