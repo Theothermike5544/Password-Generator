3b613c99c317eef2644c39690a0d04560acbd623
Michael Bishop Work PC <mrbishop5544@gmail.com>
2022-06-29 15:43:54 -0400

    creation of characters accepted and first password length check with prompts working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,21 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+// start of function code for declarations, prompts, checks, run cases
+function generatePassword() {
 
-// Start of password generating code and prompts
-function generatePassword {
+    // Arrays for defining chars, numbers, symbols, etc...
+    var upperChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    var lowerChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    var symbols = ["!", "#", "$", "%", "&", "@", "(", ")", "*", "+", ",", "-", ".", "/", "=", "^", "_", "`", "~"];
+    var siteChoice = [];
 
-  // Array list for all Characters / Numbers / Symbols acceptable by type
-var uppers = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
-var lowers = ["abcdefghijklmnopqrstuvwxyz"];
-var numbers = ["0123456789"];
-var symbols = ["!@#$%^&*()_-[]{}"];
-
-  // Initial Window Prompt asking about password length
-  var passLength = window.prompt("How many characters will the password length be? Please select a length between 8 and 128 characters long!");
-
-
-
+    // Prompts for confirming what length and chars / symbols / numbers to use
+    var charLength = window.prompt("How long would you like your password? Choose a length from 8 - 128 characters long!")
+    if (isNaN(charLength) || charLength < 8 || charLength > 128) {
+      window.alert("The required password must be between 8 and 128 characters in length. Please input a validated response and try again!");
+      return generatePassword();
+    }
 }
 
 // Write password to the #password input
